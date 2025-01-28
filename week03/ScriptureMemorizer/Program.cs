e6fde154429f4ca0c4862866f89bb01a6072fab3
rachealkatono <you@example.com>
2025-01-29 01:56:14 +0300

    week 3 new

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -11,6 +11,10 @@ class Program
             new Scripture(new Reference("Mosiah", 2, 17), "When ye are in the service of your fellow beings ye are only in the service of your God."),
             new Scripture(new Reference("Helaman", 5, 12), "It is upon the rock of our Redeemer, who is Christ, the Son of God, that ye must build your foundation."),
             new Scripture(new Reference("3 Nephi", 11, 29), "He that hath the spirit of contention is not of me, but is of the devil."),
+            new Scripture(new Reference("Proverbs", 3, 5, 6), "Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight."),
+            new Scripture(new Reference("Isaiah", 41, 10), "So do not fear, for I am with you; do not be dismayed, for I am your God. I will strengthen you and help you; I will uphold you with my righteous right hand."),
+            new Scripture(new Reference("Philippians", 4, 6, 7), "Do not be anxious about anything, but in every situation, by prayer and petition, with thanksgiving, present your requests to God. And the peace of God, which transcends all understanding, will guard your hearts and your minds in Christ Jesus."),
+            new Scripture(new Reference("Matthew", 11, 28, 30), "Come to me, all you who are weary and burdened, and I will give you rest. Take my yoke upon you and learn from me, for I am gentle and humble in heart, and you will find rest for your souls. For my yoke is easy and my burden is light.")
         };
 
         var random = new Random();
