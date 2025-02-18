6035f046ddeb583cb94642eaeae57db7c5a08cd0
rachealkatono <you@example.com>
2025-02-18 19:24:51 +0300

    final submit

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -9,10 +9,10 @@ class Program
         var running = new Running(new DateTime(2025, 09, 2), 30, 3.0); // 30 min running with 3 miles distance
         var cycling = new Cycling(new DateTime(2025, 09, 2), 45, 12.0); // 45 min cycling with 12 mph speed
         var swimming = new Swimming(new DateTime(2025, 09, 2), 30, 20); // 30 min swimming with 20 laps
-        var hiking = new Hiking(new DateTime(2025, 09, 2), 60, 500); // 60 min hiking with 500 meters elevation gain
+      
 
         // Put them in a list
-        List<Activity> activities = new List<Activity> { running, cycling, swimming, hiking };
+        List<Activity> activities = new List<Activity> { running, cycling, swimming, };
 
         // Display summaries
         foreach (var activity in activities)
